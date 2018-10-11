269f75a776411dbbd5bcddb525063f776d968b60
skyer1004 <44064549+skyer1004@users.noreply.github.com>
2018-10-11 14:57:18 -0400

    Update google-part-1.java

--- google-part-1.java
+++ google-part-1.java
@@ -876,7 +876,7 @@ public class AutocompleteSystem {
  * List<String> param_1 = obj.input(c);
  */
 
-// 428. Serialize and Deserialize N-ary Tree
+// 20 // 428. Serialize and Deserialize N-ary Tree
 // Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
 // Design an algorithm to serialize and deserialize an N-ary tree. An N-ary tree is a rooted tree in which each node has no more than N children. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that an N-ary tree can be serialized to a string and this string can be deserialized to the original tree structure.
 class Codec {
